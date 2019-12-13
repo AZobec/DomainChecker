19ee151113c23c4685438ff150da56cd973f9ca5
AZobec <arnozobec@gmail.com>
2019-12-13 12:01:39 +0100

    refacto Change checker

--- domainchecker.py
+++ domainchecker.py
@@ -10,14 +10,14 @@ import json
 #es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 #https://towardsdatascience.com/getting-started-with-elasticsearch-in-python-c3598e718380
 
-def store_record(elastic_object, index_name, record):
+def es_store_record(elastic_object, index_name, record):
     try:
         outcome = elastic_object.index(index=index_name, doc_type='_doc', body=record)
     except Exception as ex:
         print('Error in indexing data')
         print(str(ex))
 
-def create_index(es_object, index_name):
+def es_create_index(es_object, index_name):
     created = False
     # index settings
     settings = {
@@ -86,6 +86,7 @@ def createDB(hosts_file,database_file):
                     #if it worked
                         ip = str(answer)
                         ip_list.append(ip)
+                    ip_list.sort()
                 except:
                     print("error in A resolver block - maybe no A record?")
                 if  ip_list:
@@ -108,7 +109,7 @@ def createDB(hosts_file,database_file):
                         #if it worked
                         mx = str(answer).split(" ")[1]
                         mx_list.append(mx)
-                        mx_list.sort()
+                    mx_list.sort()
                 except:
                     print("error in MX resolver block - maybe no MX record ?")
                 if mx_list:
@@ -131,7 +132,7 @@ def createDB(hosts_file,database_file):
                         ns = str(answer)
                         print(domain+" "+ns)
                         ns_list.append(ns)
-                        ns_list.sort()
+                    ns_list.sort()
                 except:
                     print("error in NS resolver block - maybe no NS record?")
                 if ns_list:
@@ -149,117 +150,92 @@ def createDB(hosts_file,database_file):
                 
             domainFile.close()							
 
-def checkChange(hosts_file,database_file,es_host):
-    if es_host != False:
-        es = Elasticsearch([{'host': es_host, 'port': 9200}])
-        daytime = datetime.datetime.now().strftime("%Y-%m-%d")
-        es_index_name = "domainchecker-"+daytime
-        create_index(es,es_index_name)
+def es_connection(es_host,es_port):
+    es = Elasticsearch([{'host': es_host, 'port': es_port}])
+    return es
+
+
+def checkRecord(database_file,es_host,es_port,es_index_name,domain,checked_list,recordtype):
+    print(">>>> Refacto -  checkrecord")
     epochdate = int(datetime.datetime.now().timestamp())
     connect = lite.connect(database_file)
+    if es_host != False:
+        es = es_connection(es_host,es_port)
     with connect:
         connect.row_factory = lite.Row
         cur = connect.cursor()
-        with open(hosts_file, 'r') as domainFile:
-            for domain in domainFile:
-                domain = domain[:-1]
-                ip_list=list()
-                mx_list=list()
-                ns_list=list()
-                #For A records 
-                try:
-                    answersA = dns.resolver.query(domain, 'A')
-                    for answer in answersA:		
-                        ip = str(answer)
-                        ip_list.append(ip)
-                except:
-                    print("error in A resolver block - maybe no A record ?")
-                if ip_list :
-                        #Before need to check if  exists
-                        request = "SELECT domain,record,recordtype,updatedate FROM domains WHERE domain=\""+domain+"\" AND recordtype = \"A\""
-                        cur.execute(request)
-                        rows = cur.fetchall()
-                        for row in rows:
-                                #for each host to check
-                                #print ("{} {} {} {}".format(row["domain"], row["record"], row["recordtype"], row["updatedate"]))
-                                if str(ip_list) not in row["record"] :
-                                        logging = {}
-                                        logging = {'domain':domain, 'record_old': row["record"], 'record_new':str(ip_list),'last_seen':row['updatedate'], 'timestamp':str(epochdate), 'recordtype': row["recordtype"]}
-                                        #print(json.dumps(logging))
-                                        record = json.dumps(logging)
-                                        if es_host != False:
-                                            store_record(es,es_index_name,record)
-                                        #print("Change detected on \""+domain+"\" - New IP is : \""+str(ip_list)+"\" - Record updated")
-                                        #Now need to update IP
-                                        request = "UPDATE domains SET record =\""+str(ip_list)+"\"  WHERE domain = ? AND recordtype = ?"
-                                        cur.execute(request,(domain,"A"))
-                                        request = "UPDATE domains SET updatedate =\""+str(epochdate)+"\"  WHERE domain = ? AND recordtype = ?"
-                                        cur.execute(request,(domain,"A"))
-                try:
-                    answersMX = dns.resolver.query(domain,'MX')
-                    for answer in answersMX: 
-                        #print(str(answer).split(" ")[1])
-                        #if it worked
-                        mx = str(answer).split(" ")[1]
-                        mx_list.append(mx)
-                        mx_list.sort()
-                except:
-                    print("error in MX resolver block- maybe no MX record ?")
-                if mx_list :
-                        #Before need to check if  exists
-                        request = "SELECT domain,record,recordtype,updatedate FROM domains WHERE domain=\""+domain+"\" AND recordtype = \"MX\""
-                        cur.execute(request)
-                        rows = cur.fetchall()
-                        for row in rows:
-                                #for each host to check
-                                #print ("{} {} {} {}".format(row["domain"], row["record"], row["recordtype"], row["updatedate"]))
-                                if str(mx_list) not in row["record"] :
-                                        logging = {}
-                                        logging = {'domain':domain, 'record_old': row["record"], 'record_new':str(mx_list),'last_seen':row['updatedate'], 'timestamp':str(epochdate), 'recordtype': row["recordtype"]}
-                                        #print(json.dumps(logging))
-                                        record = json.dumps(logging)
-                                        if es_host != False:
-                                            store_record(es,es_index_name,record)
-                                        #print("Change detected on \""+domain+"\" - New MX is : \""+str(mx_list)+"\" - Record updated")
-                                        #Now need to update IP
-                                        request = "UPDATE domains SET record =\""+str(mx_list)+"\"  WHERE domain = ? AND recordtype = ?"
-                                        cur.execute(request,(domain,"MX"))
-                                        request = "UPDATE domains SET updatedate =\""+str(epochdate)+"\"  WHERE domain = ? AND recordtype = ?"
-                                        cur.execute(request,(domain,"MX"))
-                #For NS records
-                try:
-                    answersNS = dns.resolver.query(domain,'NS')
-                    for answer in answersNS: 
-                    #if it worked
-                        ns = str(answer)
-                        ns_list.append(ns)
-                        ns_list.sort()
-                except:
-                    print("error in NS resolver block - maybe no NS record ?")
-                if ns_list :
-                        #Before need to check if  exists
-                        request = "SELECT domain,record,recordtype,updatedate FROM domains WHERE domain=\""+domain+"\" AND recordtype = \"NS\""
-                        cur.execute(request)
-                        rows = cur.fetchall()
-                        for row in rows:
-                                #for each host to check
-                                #print ("{} {} {} {}".format(row["domain"], row["record"], row["recordtype"], row["updatedate"]))
-                                if str(ns_list) not in row["record"] :
-                                        logging = {}
-                                        logging = {'domain':domain, 'record_old': row["record"], 'record_new':str(ns_list),'last_seen':row['updatedate'], 'timestamp':str(epochdate), 'recordtype': row["recordtype"]}
-                                        #print(json.dumps(logging))
-                                        record = json.dumps(logging)
-                                        if es_host != False:
-                                            store_record(es,es_index_name,record)
-                                        #print("Change detected on \""+domain+"\" - New IP is : \""+str(ns_list)+"\" - Record updated")
-                                        #Now need to update IP
-                                        request = "UPDATE domains SET record =\""+str(ns_list)+"\"  WHERE domain = ? AND recordtype = ?"
-                                        cur.execute(request,(domain,"NS"))
-                                        request = "UPDATE domains SET updatedate =\""+str(epochdate)+"\"  WHERE domain = ? AND recordtype = ?"
-                                        cur.execute(request,(domain,"NS"))
-            domainFile.close()
+        request = "SELECT domain,record,recordtype,updatedate FROM domains WHERE domain=\""+domain+"\" AND recordtype = \""+recordtype+"\""
+        cur.execute(request)
+        rows = cur.fetchall()
+        for row in rows:
+                #for each host to check
+                if str(checked_list) not in row["record"] :
+                        logging = {}
+                        logging = {'domain':domain, 'record_old': row["record"], 'record_new':str(checked_list),'last_seen':row['updatedate'], 'timestamp':str(epochdate), 'recordtype': row["recordtype"]}
+                        #print(json.dumps(logging))
+                        record = json.dumps(logging)
+                        if es_host != False:
+                            es_store_record(es,es_index_name,record)
+                        else:
+                           print(json.dumps(logging)) 
+                        #print("Change detected on \""+domain+"\" - New MX is : \""+str(mx_list)+"\" - Record updated")
+                        #Now need to update IP
+                        request = "UPDATE domains SET record =\""+str(checked_list)+"\"  WHERE domain = ? AND recordtype = ?"
+                        cur.execute(request,(domain,recordtype))
+                        request = "UPDATE domains SET updatedate =\""+str(epochdate)+"\"  WHERE domain = ? AND recordtype = ?"
+                        cur.execute(request,(domain,recordtype))
+
+def listgenerator(domain,recordtype):
+    listgenerator=list()
+    if recordtype == "MX":
+        try:
+            answersMX = dns.resolver.query(domain,'MX')
+            for answer in answersMX: 
+                #print(str(answer).split(" ")[1])
+                #if it worked
+                mx = str(answer).split(" ")[1]
+                listgenerator.append(mx)
+            listgenerator.sort()
+            return (listgenerator)
+        except:
+            print("error in MX resolver block- maybe no MX record ?")
+    if recordtype == "A":
+        try:
+            answersA = dns.resolver.query(domain, 'A')
+            for answer in answersA:		
+                ip = str(answer)
+                listgenerator.append(ip)
+            listgenerator.sort()
+            return (listgenerator)
+        except:
+            print(">>> error in A resolver block - maybe no A record ?")
+    if recordtype == "NS":
+        try:
+            answersNS = dns.resolver.query(domain, 'NS')
+            for answer in answersNS:		
+                ns = str(answer)
+                listgenerator.append(ns)
+            listgenerator.sort()
+            return (listgenerator)
+        except:
+            print(">>> error in NS resolver block - maybe no NS record ?")
 
 
+def checkChangerefacto(hosts_file,database_file,es_host,es_port,es_index_name):
+    print(">>> Refacto - checkChange")
+    with open(hosts_file, 'r') as domainFile:
+        for domain in domainFile:
+            domain = domain[:-1]
+            mx_list = listgenerator(domain,"MX")
+            checkRecord(database_file,es_host,es_port,es_index_name,domain,mx_list,"MX")
+            ip_list = listgenerator(domain, "A")
+            checkRecord(database_file,es_host,es_port,es_index_name,domain,ip_list,"A")
+            ns_list = listgenerator(domain,"NS")
+            checkRecord(database_file,es_host,es_port,es_index_name,domain,ns_list,"NS")
+            
+            
+
+           
 
 if __name__ == "__main__":
     
@@ -270,8 +246,9 @@ if __name__ == "__main__":
     parser.add_option("-f", "--host-file", dest = 'hosts_file', help = "Hosts to monitor", metavar = "FILE", default = False)
     parser.add_option("-c", "--create-db-in-file", dest = 'create_db', help = "Create DB in specified file if first run", metavar = "DB", default = False)
     parser.add_option("-b", "--database", dest = 'database', help = "DB file to use for checking", metavar = "DB", default = False)
-    parser.add_option("-o", "--output", dest= 'output', help="Precise Elasticsearch host if wanted or don't use this option -", metavar="output", default=False) 
-
+    parser.add_option("-e", "--elasticsearch-host", dest= 'elasticsearch', help="Precise Elasticsearch host if wanted or don't use this option -", metavar="output", default=False) 
+    parser.add_option("-p", "--port", dest = 'port', help = 'Elasticsearch Port', metavar='PORT' ,default = False)
+    parser.add_option("-i", "--index-name", dest='index', help='Elasticsearch Index Name. If false default will be domainchecker-YYYY-MM-DD', metavar='INDEX', default = False)
     options,args = parser.parse_args()
 
     if options.hosts_file != False:
@@ -280,10 +257,24 @@ if __name__ == "__main__":
         arguments["create_db"] = options.create_db
     if options.database != False:
         arguments["database"] = options.database
-    if options.output != False:
-        arguments["output"] = options.output
-    elif options.output == False:
-        arguments["output"] = False
+    if options.elasticsearch != False:
+        arguments["elasticsearch"] = options.elasticsearch
+    elif options.elasticsearch == False:
+        arguments["elasticsearch"] = False
+    if options.port != False:
+        try:
+            arguments["port"] = int(options.port)
+        except:
+            print("[-] Port is not an integer")
+            exit()
+    elif options.port == False:
+        arguments["port"] = 9200
+    if options.index != False:
+        arguments["index"] = options.index
+    elif options.index == False:
+        daytime = datetime.datetime.now().strftime("%Y-%m-%d")
+        es_index_name = "domainchecker-"+daytime
+        arguments["index"] = es_index_name
 
     if len(sys.argv)==1:
         parser.print_help()
@@ -293,7 +284,7 @@ if __name__ == "__main__":
             createDB(arguments["hosts_file"],arguments["create_db"])
             exit()
         elif ((options.database != False) and (options.hosts_file != False)):
-            checkChange(arguments["hosts_file"],arguments["database"],arguments["output"])
+            checkChangerefacto(arguments["hosts_file"],arguments["database"],arguments["elasticsearch"],arguments["port"],arguments["index"])
             exit()
         else :
             print("Usage : python3 domainchecker.py [options]")
